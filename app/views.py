bc090334ebc204fe7e8718f52236d2f7a7c2e8af
andyjohn23 <andrewnick906@gmail.com>
2020-10-12 23:41:44 +0300

    updated files

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app
 @app.route('/')
 def index():
 
-    '''
+    """
     View root page function that returns the index page and its data
-    '''
+    """
     return render_template('index.html')
