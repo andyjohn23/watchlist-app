0d7b34489a1cd151a95601115e42d233fa3ef3c3
andyjohn23 <andrewnick906@gmail.com>
2020-10-23 01:49:21 +0300

    updated files

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://andrewjohn:andy1234@localhost/watchlist_test'
