fb148ad1c1b156fff96c52be2749c1546979b3c5
andyjohn23 <andrewnick906@gmail.com>
2020-10-24 20:31:37 +0300

    updated files

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MOVIE_API_KEY="9e90913f39ae9ea71d2f5d4bcaf30d47"
-export SECRET_KEY="<Flask WTF Secret Key>"
-export MAIL_USERNAME="andrewjohn804@gmail.com"
-export MAIL_PASSWORD="andyjohn@1996"
-
-python3.6 manage.py server
